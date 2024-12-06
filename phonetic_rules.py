3d25023f4510857a59a71c357c2f5dd286332682
mm-89 <michele.marro@unige.ch>
2024-12-06 10:23:16 +0100

    update with epitram rules

--- phonetic_rules.py
+++ phonetic_rules.py
@@ -1,21 +1,25 @@
 # phonetic maps based on
 # Glossaire des patois de la Suisse romande
-# https://portail-gpsr.unine.ch/apex/f?p=101:23:16827242380650::NO:20:::
+# https://portail-gpsr.unine.ch/apex/f?p=101:23:15555252395139::NO:20:::
 
 # some letters are unwriteble (e.g. a with a point above)
 # some letter sounds are 'between' something, so
 # there is not a precise phonetic for them
 
+# Notes:
+#
+# Strange signs like  are automatically translate
+# into a specific sign. You don't need to comment them.
+# Same thind for sound 'between' something
+
+# Commented lines are already checked with epitram to give
+# the right results so than you don't need to replace
+
 phonetic = {
-    'a': '/a/',
-    'è': '/ɛ/',
-    'é': '/e/',
-    'i': '/i/',
-    'u': '/u/',
-    'X': '/a̞/',  # Tra "a" e "ò" ????
-    'X': '/æ/',  # Tra "a" e "è" ????
-    'e': '/ə/',   # Vocale media
-    'ë': '/ɛ̈/',  # Tra "è" e /ə/
+    '': '/a̞/',  # Tra "a" e "ò" ????
+    '': '/æ/',  # Tra "a" e "è" ????
+    'e': '/ə/',   # Vocale media ????
+    'ë': '/ɛ̈/',  # Tra "è" e /ə/ ????
     'ə': '/ə/',   # Vocale assordita
     'ì': '/ɪ/',   # Tra "é" e "i"
     'ò': '/ɔ/',   # "o" aperto
@@ -51,21 +55,21 @@ phonetic = {
     'ā': '/aː/',    # Vocale lunga
     'ē': '/eː/',    # Vocale lunga
     'b': '/b/',     # Consonante "b"
-    'ch': '/ʃ/',    # Come in francese
-    'd': '/d/',     # Come in francese
-    'f': '/f/',     # Come in francese
-    'j': '/ʒ/',     # Come in francese
-    'k': '/k/',     # Come in francese
-    'l': '/l/',     # Come in francese
-    'm': '/m/',     # Come in francese
-    'n': '/n/',     # Come in francese
-    'p': '/p/',     # Come in francese
-    'r': '/ʁ/',     # Come in francese (uvulare)
-    't': '/t/',     # Come in francese
-    'v': '/v/',     # Come in francese
-    'z': '/z/',     # Come in francese
-    'dj': '/dʒ/',   # Come in italiano
-    'dz': '/dz/',   # Come in italiano
+#    'ch': '/ʃ/',    # Come in francese
+#    'd': '/d/',     # Come in francese
+#    'f': '/f/',     # Come in francese
+#    'j': '/ʒ/',     # Come in francese
+#    'k': '/k/',     # Come in francese
+#    'l': '/l/',     # Come in francese
+#    'm': '/m/',     # Come in francese
+#    'n': '/n/',     # Come in francese
+#    'p': '/p/',     # Come in francese
+#    'r': '/ʁ/',     # Come in francese (uvulare)
+#    't': '/t/',     # Come in francese
+#    'v': '/v/',     # Come in francese
+#    'z': '/z/',     # Come in francese
+#    'dj': '/dʒ/',   # Come in italiano
+#    'dz': '/dz/',   # Come in italiano
     'g': '/g/',     # Sempre occlusiva (fr. "gai")
     'h': '/h/',     # Come in tedesco (fr. "Hund")
     'ḥ': '/χ/',     # Come in tedesco (fr. "ich")
@@ -77,11 +81,11 @@ phonetic = {
     's': '/s/',     # Sempre sorda
     'ṣ': '/θ/',     # Come in inglese (es. "thing")
     '': '/ʎ̞/',    # Tra /ṣ/ e /ly/
-    'tch': '/tʃ/',  # Come in italiano (es. "cento")
-    'ts': '/ts/',   # Come in tedesco (es. "Zeit")
+#    'tch': '/tʃ/',  # Come in italiano (es. "cento")
+#    'ts': '/ts/',   # Come in tedesco (es. "Zeit")
     'y': '/j/',     # Semivocale (fr. "yeux")
     'ẓ': '/ð/',     # Come in inglese (es. "father")
-    '': '/ʁ̞/',    # Tra /ẓ/ e /ly/
-    '': '/ː/',     # Consonanti prolungate
-    '': '/ː/',     # Consonanti prolungate
+#    '': '/ʁ̞/',    # Tra /ẓ/ e /ly/
+#    '': '/ː/',     # Consonanti prolungate
+#    '': '/ː/',     # Consonanti prolungate
 }
